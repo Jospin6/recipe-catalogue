c972883807c171bcc23553f7ecb62f8a34932687
jospin6 <jospinndagano1@gmail.com>
2024-05-21 10:06:07 +0200

    Design the card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,9 @@
 
 export const Card = () => {
-    return <div className="w-[400px] h-[200px] rounded-lg box-shadow">
-
+    return <div className="w-[400px] h-[300px] rounded-lg box-shadow">
+        <div className="rounded-t-lg bg-red-300 h-[250px]"></div>
+        <div className="rounded-b-lg h-[50px] leading-[50px] text-[18px] pl-2">
+            the comp text
+        </div>
     </div>
 }
