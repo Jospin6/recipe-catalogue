989ee116b2649def9a311bd9c60128e0367a8a8f
jospin6 <jospinndagano1@gmail.com>
2024-05-21 09:54:43 +0200

    Include the navbar on the main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Card } from '@/components/Card'
+import { Navbar } from '@/components/Navbar';
 
 export default function Home() {
   return (
     <main className="text-black">
-      <Card/>
+      <Navbar/>
+      <div className='md:w-[80%] md:m-[auto]'>
+        <Card/>
+      </div>
     </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 
 export const Navbar = () => {
-    return <div className="flex justify-between items-center">
-
+    return <div className="flex justify-between items-center md:px-[50px] h-[60px]">
+        <div className="text-xl font-[500]">Recipe</div>
     </div>
 }
