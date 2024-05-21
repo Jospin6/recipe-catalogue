948e0e39a4c0a4a0cf813aefbca1ec0ff3f42407
jospin6 <jospinndagano1@gmail.com>
2024-05-21 11:15:32 +0200

    Reduce the head title size

--- src/components/HeadTitle.tsx
+++ src/components/HeadTitle.tsx
@@ -1,6 +1,6 @@
 
 export const HeadTitle = ({text, className}: {text: string, className: string}) => {
-    return <div className={`w-full h-[50px] text-center text-[25px] ${className}`}>
+    return <div className={`w-full h-[50px] text-center text-[20px] ${className}`}>
         { text }
     </div>
 }
