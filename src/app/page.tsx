65a3994f3952f82dd341d0405eacb1a20f5d743a
jospin6 <jospinndagano1@gmail.com>
2024-05-22 12:01:33 +0200

    Remove unused object properties

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function Home() {
-  const {loading, categories, error} = useSelector((state: RootState) => state.categories)
+  const {loading, categories} = useSelector((state: RootState) => state.categories)
   const dispatch = useDispatch<AppDispatch>()
   useEffect(() => {
     dispatch(fetchCategories())
@@ -27,7 +27,7 @@ export default function Home() {
     <main className="text-black">
       <HeadTitle text='Explore our meals categories' className='mb-4' />
       <div className="md:grid md:grid-cols-6 md:gap-4">
-        {categories.map((category, key )=> (
+        {categories.map((category, key) => (
           <Link href={`/category/${category.strCategory}`} className='col-span-2' key={key}>
             <Card 
               className='h-[300px]' 
