accde383c4aee7591eb4d0fbc0d150793c416c83
jospin6 <jospinndagano1@gmail.com>
2024-05-22 10:06:29 +0200

    Add extrareducers to fetch data from the mealDB

---
+++ src/features/categoriesSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
+
+interface initialState {
+    loading: boolean,
+    categories: string[],
+    error: string | null
+}
+
+const initialState: initialState = {
+    loading: false,
+    categories: [],
+    error: null
+}
+
+export const fetchCategories = createAsyncThunk("categories/fetchCategories", async () => {
+    return await fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+        .then(response => response)
+        .then(data => data)
+        .catch(error => error.message)
+})
+
+const categories = createSlice({
+    name: "categories",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchCategories.pending, (state) => {
+            state.loading = true
+        })
+        .addCase(fetchCategories.fulfilled, (state, action: PayloadAction<any>) => {
+            state.loading = false
+            state.categories = action.payload.categories
+        })
+        .addCase(fetchCategories.rejected, (state, action: PayloadAction<any>) => {
+            state.loading = false
+            state.error = action.payload
+        })
+    }
+})
+
+
+export default categories.reducer
