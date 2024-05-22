e28d6c11c82f6c84c7bd35c25543767d84c182a3
jospin6 <jospinndagano1@gmail.com>
2024-05-22 10:22:42 +0200

    Fetch  data from the mealDB api

---
+++ src/features/categorySlice.ts
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
+
+interface initialState {
+    loading: boolean,
+    category: string[],
+    error: string | null
+}
+
+const initialState: initialState = {
+    loading: false,
+    category: [],
+    error: null
+}
+
+export const fetchCategory = createAsyncThunk("category/fetchCategory", async (term: string) =>{
+    return await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${term}`)
+        .then(response => response)
+        .then(data => data)
+        .catch(error => error.message)
+})
+
+const category = createSlice({
+    name: "category",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchCategory.pending, (state) => {
+            state.loading = true
+        })
+        .addCase(fetchCategory.fulfilled, (state, action: PayloadAction<any>) => {
+            state.loading = false
+            state.category = action.payload.meals
+        })
+        .addCase(fetchCategory.rejected, (state, action: PayloadAction<any>) => {
+            state.loading = false
+            state.error = action.payload
+        })
+    }
+})
+
+
+export default category.reducer
