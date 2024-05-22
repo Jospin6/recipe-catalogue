6eb740995f59a4b939d86ab3252f95067170a3fe
jospin6 <jospinndagano1@gmail.com>
2024-05-22 10:40:27 +0200

    Add extra reducers to fetch single meals from the mealDB api

---
+++ src/features/mealsSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
+
+interface initialState {
+    loading: boolean,
+    meal: string[],
+    error: string | null
+}
+
+const initialState: initialState = {
+    loading: false,
+    meal: [],
+    error: null
+}
+
+export const fetchMeal = createAsyncThunk("meals/fetchMeal", async (id: string) => {
+    return await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then(response => response)
+        .then(data => data)
+        .catch(error => error)
+})
+
+const meals = createSlice({
+    name: "meals",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchMeal.pending, (state) => {
+            state.loading = true
+        })
+        .addCase(fetchMeal.fulfilled, (state, action: PayloadAction<any>) => {
+            state.loading = false
+            state.meal = action.payload.meals
+        })
+        .addCase(fetchMeal.rejected, (state, action: PayloadAction<any>) => {
+            state.loading = false
+            state.error = action.payload
+        })
+    }
+})
+
+export default meals.reducer
