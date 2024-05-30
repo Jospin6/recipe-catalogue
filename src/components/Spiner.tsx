8fdffebd72d06f174734243bf88db3b4f46f3260
jospin6 <jospinndagano1@gmail.com>
2024-05-30 12:03:38 +0200

    Dsplay loading gif in the spiner

--- src/components/Spiner.tsx
+++ src/components/Spiner.tsx
@@ -1,9 +1,8 @@
 import loading from '@/assets/loading.gif'
-import { StaticImageData } from 'next/image';
 
 
 export const Spiner = () => {
     return <div className="flex items-center justify-center w-full h-[70vh]">
-        
+        <img src={loading.src} alt="Loading..." />
     </div>
 }
