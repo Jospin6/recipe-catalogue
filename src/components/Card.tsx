53933f4c52121166295ddb8c1f89799441a11ab0
jospin6 <jospinndagano1@gmail.com>
2024-08-13 10:11:36 +0200

    Configure nextJs image

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 export const Card = ({className, src, title}: {className: string, src: string, title: string}) => {
     return <div className={`rounded-lg box-shadow mb-4 md:mb-[0px] ${className}`}>
         <div className="rounded-t-lg h-[85%]">
-            <Image src={src} alt="meal" className="rounded-t-lg w-full h-full"/>
+            <Image src={src} alt="meal" className="rounded-t-lg w-full h-full" width="500" height={"500"}/>
         </div>
         <div className="rounded-b-lg h-[15%] flex items-center text-[18px] pl-2">
             {title}
